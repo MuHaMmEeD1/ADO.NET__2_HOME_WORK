d6c24dee5d8f2373521364a5cbbd444b3cfa2cb9
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2024-01-25 01:55:11 +0400

    Add project files.

---
+++ ADO.NET__2_HOME_WORK/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ADO.NET__2_HOME_WORK
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ADO.NET__2_HOME_WORK/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Data.SqlClient;
+
+namespace ADO.NET__2_HOME_WORK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public DataTable DataTable { get; set; } 
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            SqlConnection sql = new SqlConnection("Data Source=DESKTOP-RV3QOTA\\SQLEXPRESS;Initial Catalog=\"Library \";Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Authors", sql);
+            DataTable = new DataTable();
+            sqlDataAdapter.Fill(DataTable);
+
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            DataTable.Clear();
+
+            SqlConnection sql = new SqlConnection("Data Source=DESKTOP-RV3QOTA\\SQLEXPRESS;Initial Catalog=\"Library \";Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            //SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Authors AS A WHERE A.FirstName = @name", sql);
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Authors AS A WHERE A.FirstName Like @name", sql);
+
+            string name = "%"+ComboBox_1.Text + "%";
+            sqlCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NVarChar) { Value = name });
+
+            sqlCommand.Parameters["@name"].SourceColumn = "FirstName";
+            sqlCommand.Parameters["@name"].SourceVersion = DataRowVersion.Current;
+   
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+            sqlDataAdapter.Fill(DataTable);
+
+
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (SqlConnection sql = new SqlConnection("Data Source=DESKTOP-RV3QOTA\\SQLEXPRESS;Initial Catalog=\"Library \";Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False")) { 
+
+
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Authors", sql);
+            SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+            sqlDataAdapter.Update(DataTable);
+        }
+
+     
+        }
+    }
+}
+
+
